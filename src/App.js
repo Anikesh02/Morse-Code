d2f4052f7b80cc25ff24200db6abc2cac78699d1
Anikesh02 <anikeshkulalinft-@atharvacoe.ac.in>
2023-11-13 08:59:09 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState, useEffect } from 'react';
+import './App.css'
 function App() {
+  const [text, setText] = useState('');
+  const [morseCode, setMorseCode] = useState('');
+
+  useEffect(() => {
+    // Define the Morse code mappings
+    const morseCodeMappings = {
+      A: '.-', B: '-...', C: '-.-.', D: '-..', E: '.',
+      F: '..-.', G: '--.', H: '....', I: '..', J: '.---',
+      K: '-.-', L: '.-..', M: '--', N: '-.', O: '---',
+      P: '.--.', Q: '--.-', R: '.-.', S: '...', T: '-',
+      U: '..-', V: '...-', W: '.--', X: '-..-', Y: '-.--',
+      Z: '--..', ' ': '|'
+    };
+
+    // Function to translate text to Morse code
+    const translateToMorseCode = (inputText) => {
+      const sanitizedText = inputText.toUpperCase();
+      const words = sanitizedText.split(' ');
+      const morseCodeWords = words.map((word) => {
+        const characters = word.split('');
+        const morseCodeCharacters = characters.map((char) => {
+          return morseCodeMappings[char] || '';
+        });
+        return morseCodeCharacters.join(' ');
+      });
+      return morseCodeWords.join('   '); // Use 3 spaces to separate words
+    };
+
+    
+    setMorseCode(translateToMorseCode(text));
+  }, [text]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Morse Code Translator</h1>
+      <textarea
+        rows="4"
+        cols="50"
+        placeholder="Enter text..."
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+       <h2>Morse Code:</h2>
+        <p>{morseCode}</p>
+      
     </div>
   );
 }
